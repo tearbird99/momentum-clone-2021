e9fb7a77e37dfe69ebd30a9af06d118feb3ab614
tearbird99 <tearbird99@naver.com>
2021-11-08 20:04:45 +0900

    #2.3 const and let

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const a = 5;
 const b = 2;
-const myName = "Eugene";
+let myName = "Eugene";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "Seo";
+console.log("my name is " + myName);
