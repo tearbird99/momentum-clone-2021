3f5376573e1c084f7bd03b074cf286cf575abf35
tearbird99 <tearbird99@naver.com>
2021-11-08 20:22:04 +0900

    #2.4 Booleans

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-const a = 5;
-const b = 2;
-let myName = "Eugene";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "Seo";
-console.log("my name is " + myName);
+const amIFat = false;
+console.log(amIFat);
