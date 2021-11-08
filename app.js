975720d95973e1f44cba0252d7208b035a036916
tearbird99 <tearbird99@naver.com>
2021-11-08 21:09:01 +0900

    #2.5 Arrays

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const amIFat = false;
-console.log(amIFat);
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat];
+console.log(daysOfWeek[1]);
+daysOfWeek.push("sun");
+
+const nonsense = [1, 2, "hello", false, null, true, undefined];
