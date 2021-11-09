dbbf592a5f2053cf8714a2420c90395cd96c4ff3
tearbird99 <tearbird99@naver.com>
2021-11-09 09:27:15 +0900

    #2.11 Returns

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const calculator = {
   plus: function (a, b) {
-    console.log(a + b);
+    return a + b;
   },
   minus: function (a, b) {
-    console.log(a - b);
+    return a - b;
+  },
+  times: function (a, b) {
+    return a * b;
   },
   divide: function (a, b) {
-    console.log(a / b);
+    return a / b;
   },
-  invol: function (a, b) {
-    console.log(a ** b);
+  power: function (a, b) {
+    return a ** b;
   },
 };
-
-calculator.plus(2, 3);
-calculator.minus(5, 1);
-calculator.divide(9, 2);
-calculator.invol(8, 3);
