d16ed2a3b1a20c8352ccb9705d0bb3ac11d19d0a
tearbird99 <tearbird99@naver.com>
2021-11-09 17:21:10 +0900

    #3.1 HTML in Javascript

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number");
-} else if (age < 18) {
-  console.log("You are too young");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise");
-} else if (age === 100) {
-  console.log("Wow you are wise");
-}
+const title = document.getElementById("title");
+console.dir(title);
+title.innerText = "Got you!";
