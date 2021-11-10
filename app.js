961979db783095128a45e682fbc69649dfd1cc72
tearbird99 <tearbird99@naver.com>
2021-11-10 13:30:44 +0900

    #3.4 Events part Two

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const title = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  console.log("title was clicked!");
+  title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+  title.innerText = "Mouse is gone!";
 }
 
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
