084a2eb34d32d73cffd95d5aea534e4412dc6ca7
tearbird99 <tearbird99@naver.com>
2021-11-14 15:05:48 +0900

    #7.1 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,20 @@
 const toDoForm = document.querySelector("#todo-form");
 const toDoImform = document.querySelector("#todo-form input");
-const toDoList = document.querySelector("#todo-List");
+const toDoList = document.querySelector("#todo-list");
+
+function paintToDo(newToDo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newToDo;
+  toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoImform.value;
   toDoImform.value = "";
+  paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
