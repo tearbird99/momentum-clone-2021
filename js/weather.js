9edb80b97b767abfcbc3033f1792972735111b96
tearbird99 <tearbird99@naver.com>
2021-11-15 09:27:02 +0900

    #8.0 Geolocation

---
+++ js/weather.js
@@ -0,0 +1,9 @@
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
