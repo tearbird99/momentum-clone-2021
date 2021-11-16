2b125bdeea80f906dea9dd632998f1341a6a348b
tearbird99 <tearbird99@naver.com>
2021-11-16 15:52:21 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,17 @@
+const API_KEY = "c15310e8c3f53d81732a61884f648b2f";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
